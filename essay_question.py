3d6956fdab1a238a65d708332a3354f02effe611
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-01-21 12:36:18 -0500

    Initial commit... with correct files

--- essay_question.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-from typing import Optional
-
-
-@dataclass(repr=False)
-class EssayQuestion:
-    pass
--- question_pool.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-from typing import Iterator
-
-from essay_question import EssayQuestion
-
-
-class QuestionPool:
-    pass
